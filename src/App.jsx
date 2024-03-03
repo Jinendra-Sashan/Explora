c733a930d0eeadbea1fd87c34bb2a047d0bdebec
Adshyan Matheetharan <w1998832@my.westminster.ac.uk>
2024-03-03 21:12:13 +0530

    Feature(Sign-In): Created The Sign In Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Welcome />} />
       </Routes>
