3c433152f825b904903563a083b4b85098ef470e
Adshyan Matheetharan <w1998832@my.westminster.ac.uk>
2024-03-05 19:20:52 +0530

    Feature(Sign-In): Created Forgot Password Form Component

--- frontend/src/components/RegistrationForm/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useState } from "react";
+import { useState } from "react";
 import { app } from "../../firebaseConfig";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
