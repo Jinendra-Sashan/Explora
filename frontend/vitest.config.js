4ea28da237c1cf3ebf5e3b27fd7ff05ac76ec4dc
Adshyan Matheetharan <w1998832@my.westminster.ac.uk>
2024-03-06 00:10:11 +0530

    Fixed: Updated 'vitest.config.js' File For Streamlined Testing

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,5 +1,7 @@
-export default {
+import { defineConfig } from "vitest";
+
+export default defineConfig({
   testMatch: "**/tests/**/*.{spec,test}.{js,jsx}",
   testEnvironment: "jsdom",
   setupFiles: ["./tests/setup.js"],
-};
+});
