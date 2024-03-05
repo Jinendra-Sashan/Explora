63e9271f8638cf005fcee47286a0248c8f797598
Adshyan Matheetharan <w1998832@my.westminster.ac.uk>
2024-03-05 19:27:13 +0530

    Feature(Sign-Up): Created The Forgot Password Page

---
+++ frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import ForgotPassword from '../components/ForgotPassword/ForgotPassword';
+
+const ForgotPassword = () => {
+    <div className="fixed left-0 top-0 z-0 h-screen w-screen bg-[url('https://cdn.dribbble.com/userupload/12608757/file/original-7c0e168619a01690aeee929e3f60cd13.jpg?resize=2400x1803')] bg-cover bg-center">
+      <div className="absolute left-0 top-0 h-full w-full bg-black/10">
+        <div className="flex h-screen flex-col items-center justify-center">
+          <ForgotPassword></ForgotPassword>
+        </div>
+      </div>
+    </div>
+}
+
+export default ForgotPassword;
