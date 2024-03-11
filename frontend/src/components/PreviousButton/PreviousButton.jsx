e9ecdf1d6da468b6cb3e7eaf2a4c8e00c2443846
Matheetharan-Adshyan <w1998832@my.westminster.ac.uk>
2024-03-12 02:28:06 +0530

    Fixed(Trip-Details): Added Responsiveness To The 'Previous Return' Button

--- frontend/src/components/PreviousButton/PreviousButton.jsx
+++ frontend/src/components/PreviousButton/PreviousButton.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const PreviousButton = ({ to }) => {
     return(
         <Link to={to}>
-            <button className="fixed top-4 left-4 rounded-full bg-black text-white dark:text-black dark:bg-white p-3 hover:bg-getstarted-dark dark:hover:bg-getstarted-light">
-                <ArrowLeft className="h-7 w-7" />
+            <button className="fixed top-6 left-6 rounded-full bg-black text-white dark:text-black dark:bg-white p-3 hover:bg-getstarted-dark dark:hover:bg-getstarted-light">
+                <ArrowLeft className="xl:h-7 xl:w-7 lg:h-7 lg:w-7 md:h-6 md:w-6 sm:h-5 sm:w-5 mobile:h-5 mobile:w-5" />
             </button>
         </Link>
     )
