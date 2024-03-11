8201ec1696e1dff4e62157cadb893ec3a9beeca3
Matheetharan-Adshyan <w1998832@my.westminster.ac.uk>
2024-03-12 03:45:18 +0530

    Fixed(Trip-Details): Added Responsiveness To The Basic Elements

--- frontend/src/pages/TripDetails.jsx
+++ frontend/src/pages/TripDetails.jsx
@@ -9,8 +9,8 @@ const TripDetails = () => {
         alt="Trip Image"
         className="h-screen w-full object-cover"
       />
-      <h1 className=" heading-animate absolute top-45 left-7 py-10 px-3 bottom-16 text-white font-primary font-semibold text-9xl">Bali, Indonesia</h1>
-      <h2 className=" subheading-animate absolute top-45 left-9 py-10 px-3 bottom-2 font-primary text-gray-100 font-semibold text-4xl">Started On February 2024</h2>
+      <h1 className=" heading-animate absolute top-45 left-7 py-10 px-3 bottom-16 text-white font-primary font-semibold xl:text-9xl lg:text-9xl md:text-8xl sm:text-7xl mobile:text-6xl">Bali, Indonesia</h1>
+      <h2 className=" subheading-animate absolute top-45 left-9 py-10 px-3 bottom-2 font-primary text-gray-100 font-semibold xl:text-4xl lg:text-4xl md:text-3xl sm:text-2xl mobile:text-2xl">Started On February 2024</h2>
     </div>
   );
 };
