ef174df75e30d94c4b84653b2b93031f755777c9
Matheetharan-Adshyan <w1998832@my.westminster.ac.uk>
2024-03-12 17:24:05 +0530

    Feature(Trip-Details): Created The 'Favourite' Button

--- frontend/src/pages/TripDetails.jsx
+++ frontend/src/pages/TripDetails.jsx
@@ -1,16 +1,22 @@
 import PreviousButton from "../components/PreviousButton/PreviousButton";
+import FavouriteButton from "../components/FavouriteButton/FavouriteButton";
 
 const TripDetails = () => {
   return (
     <div className=" fixed left-0 top-0 z-0 h-screen w-screen bg-white bg-cover bg-center dark:bg-black">
       <PreviousButton></PreviousButton>
+      <FavouriteButton></FavouriteButton>
       <img
         src="https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Trip Image"
         className="h-screen w-full object-cover"
       />
-      <h1 className=" heading-animate absolute top-45 left-7 py-10 px-3 bottom-16 text-white font-primary font-semibold xl:text-9xl lg:text-9xl md:text-8xl sm:text-7xl mobile:text-6xl">Bali, Indonesia</h1>
-      <h2 className=" subheading-animate absolute top-45 left-9 py-10 px-3 bottom-2 font-primary text-gray-100 font-semibold xl:text-4xl lg:text-4xl md:text-3xl sm:text-2xl mobile:text-2xl">Started On February 2024</h2>
+      <h1 className=" heading-animate top-45 absolute bottom-16 left-7 px-3 py-10 font-primary font-semibold text-white mobile:text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-9xl">
+        Bali, Indonesia
+      </h1>
+      <h2 className=" subheading-animate top-45 absolute bottom-2 left-9 px-3 py-10 font-primary font-semibold text-gray-100 mobile:text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">
+        Started On February 2024
+      </h2>
     </div>
   );
 };
