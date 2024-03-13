0715c688a1fc81486abc75f6c8c14ac1556b83b7
Matheetharan-Adshyan <w1998832@my.westminster.ac.uk>
2024-03-13 21:40:31 +0530

    Fixed(Trip-Details): Updated The Hovered Colors Of The Buttons In The 'Trip Page'

--- frontend/src/components/PreviousButton/PreviousButton.jsx
+++ frontend/src/components/PreviousButton/PreviousButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const PreviousButton = ({ to }) => {
   return (
     <Link to={to}>
-      <button className="fixed left-6 top-6 z-50 rounded-full bg-white p-4 text-black hover:bg-getstarted-dark dark:hover:bg-getstarted-light dark:text-white dark:bg-black">
+      <button className="fixed left-6 top-6 z-50 rounded-full bg-white p-4 text-black hover:bg-getstarted-light dark:hover:bg-getstarted-dark dark:text-white dark:bg-black">
         <ArrowLeft className="mobile:h-5 mobile:w-5 sm:h-5 sm:w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-7 xl:w-7" />
       </button>
     </Link>
--- frontend/src/components/TripInformationButton/TripInformationButton.jsx
+++ frontend/src/components/TripInformationButton/TripInformationButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const TripInformationButton = ({ to }) => {
   return (
     <Link to={to}>
-    <button className="fixed bottom-6 right-6 z-50 flex items-center rounded-full bg-white p-4 text-black hover:bg-getstarted-dark dark:hover:bg-getstarted-light dark:text-white dark:bg-black">
+    <button className="fixed bottom-6 right-6 z-50 flex items-center rounded-full bg-white p-4 text-black hover:bg-getstarted-light dark:hover:bg-getstarted-dark dark:text-white dark:bg-black">
       <MapPin className="mobile:h-5 mobile:w-5 sm:h-5 sm:w-5 md:h-6 md:w-6 lg:h-7 lg:w-7 xl:h-7 xl:w-7" />
       <span className="ml-2 font-semibold text-base font-primary px-1 sm:text-base mobile:text-sm">Show More Details</span>
     </button>
