106e0f92c90f9ff983d480617f6d3e657cd7b36a
SDhilu <sivakumar.20220371@iit.ac.lk>
2024-03-20 22:13:16 +0530

    updated the files according to the errors from the pull request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Checklist from "./pages/Checklist";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Welcome from "./pages/Welcome";
+import Trip from "./pages/Trip";
+
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/checklist" element={<Checklist />} />
-      </Routes>
-    </Router>
-  );
+ return (
+   <Router>
+     <Routes>
+     <Route path="/" element={<Welcome />} />
+       <Route path="/trip" element={<Trip />} />
+       <Route path="/signup" element={<SignUp />} />
+       <Route path="/signin" element={<SignIn />} />
+       <Route path="/forgotpassword" element={<ForgotPassword />} />
+     </Routes>
+   </Router>
+ );
 }
 
+
 export default App;
+
+
+
