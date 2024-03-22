17d5d7ce7d8b4b18d0318514657228b60a68b97e
SDhilu <sivakumar.20220371@iit.ac.lk>
2024-03-22 15:13:37 +0530

    Revert "Removed The Frontend Folder"
    This reverts commit 38cef533ccce9e04248bae1b23a66dbfa2ffa439.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Welcome from "./pages/Welcome";
+import CreateNewTrip from "./pages/CreateNewTrip";
+import Trip from "./pages/Trip";
+import HomeOverview from './pages/HomeOverview';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/newtrip" element={<CreateNewTrip />} />
+        <Route path="/trip" element={<Trip />} />
+        <Route path="/home" element={<HomeOverview />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/CreateNewTrip.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateNewTrip() {
+  return (
+    <div>
+      <h1>Create a New Trip</h1>
+      {/* Add form elements and other content for creating a trip */}
+    </div>
+  );
+}
+
+export default CreateNewTrip;
