e182e6f12adf6d73d5325b5b6952467a1cbda8a0
Adshyan Matheetharan <157170579+Matheetharan-Adshyan@users.noreply.github.com>
2024-03-24 21:30:55 +0530

    Feature(Journal): Created Journal Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import UserInformation from "./pages/UserInformation";
 import TripDetails from "./pages/TripDetails";
 import ExistingTripsList from "./pages/ExistingTripsList";
 import ExpenseTracker from "./pages/ExpenseTracker";
+import CreateJournal from "./pages/CreateJournal";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
         <Route path="/tripdetails/:tripId" element={<TripDetails />} />
         <Route path="/existingtrips" element={<ExistingTripsList />} />
         <Route path="/expenses" element={<ExpenseTracker/>} />
+        <Route path="/createnewjournal" element={<CreateJournal/>} />
       </Routes>
     </Router>
   );
