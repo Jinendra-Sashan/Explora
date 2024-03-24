bdbf0fb464cc538b946a3a61a8a0d5ac8c3f572c
Adshyan Matheetharan <157170579+Matheetharan-Adshyan@users.noreply.github.com>
2024-03-24 16:06:32 +0530

    Fixed(New-Trip): Updated The New Trip Page

---
+++ frontend/src/components/CreateNewTripOverview/CreateNewTripOverview.jsx
@@ -0,0 +1,9 @@
+const CreateNewTripOverview = () => {
+    return (
+        <div className="mx-auto max-w-lg rounded-3xl bg-white p-7 px-7 py-7 mobile:mx-auto mobile:max-w-sm md:max-w-lg md:p-10 md:px-11 md:py-11 lg:max-w-2xl dark:bg-black">
+            
+        </div>
+    )
+}
+
+export default CreateNewTripOverview;
