c371520b7ff45342b7450bee58d1559c48ba0b60
Adshyan Matheetharan <157170579+Matheetharan-Adshyan@users.noreply.github.com>
2024-03-24 17:12:18 +0530

    Fixed(Create-New-Trip): Connected To Firebase And Updated The User Interface

--- frontend/src/pages/CreateNewTrip.jsx
+++ frontend/src/pages/CreateNewTrip.jsx
@@ -6,8 +6,8 @@ const CreateNewTrip = () => {
   return (
     <div className="fixed left-0 top-0 z-0 h-screen w-screen bg-[url('https://cdn.dribbble.com/userupload/12608757/file/original-7c0e168619a01690aeee929e3f60cd13.jpg?resize=2400x1803')] bg-cover bg-center">
       <div className="absolute left-0 top-0 h-full w-full bg-black/10">
-        <NavigationLinksButton></NavigationLinksButton>
         <PreviousButton></PreviousButton>
+        <NavigationLinksButton></NavigationLinksButton>
         <div className="flex h-screen items-center justify-center">
           <CreateNewTripOverview></CreateNewTripOverview>
         </div>
